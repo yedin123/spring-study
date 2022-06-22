aab92d3c037a3d36a4869d102a1eb33ce86c94cd
yedin123 <73570623+yedin123@users.noreply.github.com>
2022-06-22 17:50:05 +0900

    add mariaDB

---
+++ src/main/java/com/example/springtest/SpringConfig.java
@@ -0,0 +1,28 @@
+package com.example.springtest;
+
+import com.example.springtest.repository.JpaMemberRepository;
+import com.example.springtest.repository.MemberRepository;
+import com.example.springtest.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+@Configuration
+
+public class SpringConfig {
+    private final DataSource dataSource;
+    private final EntityManager em;
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
+    }
+}
