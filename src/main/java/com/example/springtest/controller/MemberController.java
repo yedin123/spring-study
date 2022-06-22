5bb7cd48bc43689ab3178421cea697d9fb661533
yedin123 <73570623+yedin123@users.noreply.github.com>
2022-06-22 18:49:00 +0900

    duplicate check

--- src/main/java/com/example/springtest/controller/MemberController.java
+++ src/main/java/com/example/springtest/controller/MemberController.java
@@ -9,6 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @Controller
@@ -34,10 +38,17 @@ public class MemberController {
     }
 
     @PostMapping(value = "/members/new")
-    public String create(MemberForm form) {
+    public String create(MemberForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         Member member = new Member();
         member.setName(form.getName());
-        memberService.join(member);
+        try { //회원 중복 확인
+            memberService.join(member);
+        } catch (IllegalStateException e) {
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<script>alert('중복된 회원입니다.'); history.go(-1);</script>");
+            out.flush();
+        }
         return "redirect:/";
     }
 
